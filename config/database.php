eecf320d38e7f28326f809f2e7548e6480e81ec2
tuannghia123 <tuannghia160798@gmail.com>
2021-03-06 17:17:26 +0700

    update databse

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('postgres://vmccdshmeiqhij:d49ac8131f5761c83d6313ea6cabfe8878df90a508458f9d9a7da6697ce1240b@ec2-3-95-85-91.compute-1.amazonaws.com:5432/d1ij0c2hdkj9rm');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
